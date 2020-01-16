5f64b371f9d99509138584e50601bb16bcebc876
rachel-mclean <59896196+rachel-mclean@users.noreply.github.com>
2020-01-16 11:39:18 -0800

    Update anyPositive.js

--- exercises/arrays/anyPositive/anyPositive.js
+++ exercises/arrays/anyPositive/anyPositive.js
@@ -14,14 +14,21 @@
  *   and false otherwise.
  */
 function anyPositive(array) {
-  // This is your job. :)
+  for(let num of array){
+    if(num>0){
+      return true;
+    }
+  }
+  return false;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for anyPositive:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(anyPositive([1, 1, 1, 1])===true);
+  console.log(anyPositive([-1, -1, -1, -1])===false);
+  console.log(anyPositive([-1, 1, -1, 1])===true);
+  console.log(anyPositive([0, 0, 0])===false);
 }
 
 module.exports = anyPositive;
