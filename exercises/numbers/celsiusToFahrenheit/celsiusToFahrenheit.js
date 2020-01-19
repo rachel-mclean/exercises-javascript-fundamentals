3f44e8d60bf0109118022811334b5a93134ce264
rachel-mclean <59896196+rachel-mclean@users.noreply.github.com>
2020-01-18 21:08:56 -0800

    Update celsiusToFahrenheit.js

--- exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
+++ exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
@@ -4,14 +4,19 @@
  * @param {number} temp - The temperature in Celsius
  * @returns {boolean} The temperature in Fahrenheit
  */
+
 function celsiusToFahrenheit(temp) {
-  // This is your job. :).
+  return Math.round(temp*(9/5)+32);
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for celsiusToFahrenheit:');
 
-  // Your sanity checks go here.
+  console.log(celsiusToFahrenheit(0)===32);
+  console.log(celsiusToFahrenheit(10)===50);
+  console.log(celsiusToFahrenheit(100)===212);
+  console.log(celsiusToFahrenheit(13)===55);
+  console.log(celsiusToFahrenheit(81)===178);
 }
 
 module.exports = celsiusToFahrenheit;
