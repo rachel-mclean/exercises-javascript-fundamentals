d419952e0944258802033596a324dffe1b1bb511
rachel-mclean <59896196+rachel-mclean@users.noreply.github.com>
2020-01-18 17:05:42 -0800

    letterGrade

--- exercises/branching-conditionals/letterGrade/letterGrade.js
+++ exercises/branching-conditionals/letterGrade/letterGrade.js
@@ -14,14 +14,66 @@
  * @return {string} The corresponding letter grade for the given percentage grade
  */
 function letterGrade(percentGrade) {
-  // This is your job. :)
+  return findLetter(percentGrade) + findSymbol(percentGrade);
+}
+
+function findLetter(percentGrade){
+  let tensNumber = Math.floor(percentGrade/10);
+  if(tensNumber<6){
+    return "F";
+  }
+
+  else if(tensNumber<7){
+    return "D";
+  }
+
+  else if(tensNumber<8){
+    return "C";
+  }
+
+  else if(tensNumber<9){
+    return "B";
+  }
+
+  else if(tensNumber<=10){
+    return "A";
+  }
+}
+
+function findSymbol(percentGrade){
+  if(percentGrade===100){
+    return "+";
+  }
+
+  else if(percentGrade<60){
+    return "";
+  }
+
+  else if(percentGrade%10<=2){
+    return "-";
+  }
+
+  else if(percentGrade%10<=6){
+    return "";
+  }
+
+  else{
+    return "+";
+  }
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for letterGrade:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(letterGrade(50)==='F');
+  console.log(letterGrade(70)==='C-');
+  console.log(letterGrade(89)==='B+');
+  console.log(letterGrade(60)==='D-');
+  console.log(letterGrade(10)==='F');
+  console.log(letterGrade(100)==='A+');
+  console.log(letterGrade(99)==='A+');
+  console.log(letterGrade(0)==='F');
+  console.log(letterGrade(81)==='B-');
 }
 
 module.exports = letterGrade;
