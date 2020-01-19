63e710bcbea900403b887253099c0af57b2f3393
rachel-mclean <59896196+rachel-mclean@users.noreply.github.com>
2020-01-18 21:30:18 -0800

    Add files via upload

--- exercises/strings/capitalize/capitalize.js
+++ exercises/strings/capitalize/capitalize.js
@@ -10,15 +10,17 @@
  * @returns {string} A capitalized copy of the string
  */
 
-function capitalize(num) {
-  // This is your job. :)
+function capitalize(string) {
+  let first = string.slice(0,1).toUpperCase();
+  return first + string.slice(1);
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for capitalize:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(capitalize("dog")==="Dog");
+  console.log(capitalize("hello")==="Hello");
+  console.log(capitalize("giraffe")==="Giraffe");
 }
 
 module.exports = capitalize;
