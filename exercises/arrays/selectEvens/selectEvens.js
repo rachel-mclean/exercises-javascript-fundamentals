fd555b4d3420d5bf65cc75e22bbf371a5e9ebb05
rachel-mclean <59896196+rachel-mclean@users.noreply.github.com>
2020-01-20 07:55:26 -0800

    Add files via upload

--- exercises/arrays/selectEvens/selectEvens.js
+++ exercises/arrays/selectEvens/selectEvens.js
@@ -13,15 +13,25 @@
  * @param {number[]} array - An array of numbers
  * @returns {number[]} A new array consisting of all the even numbers in the input array
  */
-function selectEvens(array) {
-  // This is your job. :)
+function selectEvens(array) { //this is not working but I can't find my mistake
+  let evens = [];
+
+  for(let i = 0; i<array.length;i++){
+    if(array[i]%2===0){
+      evens.push(array[i]);
+    }
+  }
+
+  return evens;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectEvens:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
-}
+  console.log(selectEvens([1, 2, 3, 4, 5]) + "===[2, 4]");
+  console.log(selectEvens([10, 2, 19, 10])+ "===[10, 2, 10]");
+  console.log(selectEvens([1, 1, 1, 2])+ "===[2]");
+  console.log(selectEvens([11, 21, 31]) + "===[]");
+
 
 module.exports = selectEvens;
