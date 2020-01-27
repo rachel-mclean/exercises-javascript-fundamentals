7017cba841051a73a4315a9c8dfe0f7f0614d1e2
rachel-mclean <59896196+rachel-mclean@users.noreply.github.com>
2020-01-27 15:32:25 -0800

    selectLongerThan

--- exercises/arrays/selectLongerThan/selectLongerThan.js
+++ exercises/arrays/selectLongerThan/selectLongerThan.js
@@ -19,14 +19,38 @@
  *  strictly greater the given threshold
  */
 function selectLongerThan(array, threshold) {
-  // This is your job. :)
+  let newArray = [];
+
+  for(let i = 0; i<array.length; i++){
+    if(array[i].length>threshold){
+      newArray.push(array[i]);
+    }
+  }
+
+  return newArray;
 }
+
+function arrayEquals(array1, array2){
+  if(array1.length!==array2.length){
+    return false;
+  }
+  for(let i = 0; i<array1.length; i++){
+    if (array1[i]!==array2[i]){
+      return false;
+    }
+  }
+
+  return true;
+} 
 
 if (require.main === module) {
   console.log('Running sanity checks for selectLongerThan:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(arrayEquals(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], -1), ['', 'aaa', 'bb', 'c', 'dddd']));
+  console.log(arrayEquals(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 0), ['aaa', 'bb', 'c', 'dddd']));
+  console.log(arrayEquals(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 1), ['aaa', 'bb', 'dddd']));
+  console.log(arrayEquals(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 2), ['aaa', 'dddd']));
+  console.log(arrayEquals(selectLongerThan(['', 'aaa', 'bb', 'c', 'dddd'], 3), ['dddd']));
 }
 
 module.exports = selectLongerThan;
